e6a09cb6d81b6fc43ce150e762bc377e977ba1ce
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-08-17 20:27:52 +0200

    [backend] Fix CORS

--- backend/src/main/kotlin/com/warningimhack3r/mintstonebackend/MintstoneBackendApplication.kt
+++ backend/src/main/kotlin/com/warningimhack3r/mintstonebackend/MintstoneBackendApplication.kt
@@ -2,10 +2,25 @@ package com.warningimhack3r.mintstonebackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class MintstoneBackendApplication
 
 fun main(args: Array<String>) {
     runApplication<MintstoneBackendApplication>(*args)
+}
+
+@Configuration
+@EnableWebMvc
+class Configurer: WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+    }
 }
