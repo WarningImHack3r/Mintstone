a03be6abf68243264f5adf46795a8f4160fd2ca8
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-08-17 20:17:20 +0200

    [frontend] Add error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+<div class="flex h-full w-full flex-col items-center justify-center">
+	<p class="pb-16 text-7xl font-bold opacity-50">{$page.status}</p>
+	<h2 class="h2">{$page.error?.message}</h2>
+</div>
